a68c795ffc97afa1f991bc328652dbc46e5c7a9f
crowberios <crowberios@gmail.com>
2019-07-14 00:52:19 +0300

    MSS-4: Add PicturesPageController with "/pictures" url-mapping

---
+++ src/main/java/com/epam/converters/PicturesAlbumConverter.java
@@ -0,0 +1,14 @@
+package com.epam.converters;
+
+import com.epam.dto.PictureAlbumDto;
+import com.epam.models.PictureAlbumModel;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PicturesAlbumConverter implements Converter<PictureAlbumModel, PictureAlbumDto> {
+    @Override
+    public PictureAlbumDto convert(PictureAlbumModel pictureAlbumModel) {
+        return new PictureAlbumDto(pictureAlbumModel.getName(), pictureAlbumModel.getDescription());
+    }
+}
